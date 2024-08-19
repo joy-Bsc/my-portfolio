e7377b4bd11e03557c7a28376f4e45c1446ff165
Joy Dhar <104727780+joy-Bsc@users.noreply.github.com>
2024-08-19 15:19:53 +0600

    Update Header.jsx

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Nav from './Nav';
+import {Button} from '../ui/Button';
 import MobileNav from './MobileNav';
 import Image from 'next/image';
 const Header = () => {
